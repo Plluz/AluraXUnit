4a374e47c7defbc4a8329b028f113ca9f3e8b0d8
Pedro Lumertz <plluz@outlook.com>
2020-10-05 12:32:48 -0300

    applied the Arrange-Act-Assert pattern

--- Test/MathsTest.cs
+++ Test/MathsTest.cs
@@ -11,13 +11,29 @@ namespace AluraXUnit.Test
         [Fact]
         public void SumWorks()
         {
-            Assert.Equal(10, _maths.Sum(5, 5));
+            // Arrange
+            var values = new int[] {5, 5};
+
+            // Act
+            var result = _maths.Sum(values);
+
+            // Assert
+            var expected = 10;
+            Assert.Equal(expected, result);
         }
 
         [Fact]
         public void MultiplicationWorks()
         {
-            Assert.Equal(25, _maths.Multiply(5, 5));
+            // Arrange
+            var values = new int[] { 5, 5 };
+
+            // Act
+            var result = _maths.Multiply(values);
+
+            // Assert
+            var expected = 25;
+            Assert.Equal(expected, result);
         }
     }
 }
