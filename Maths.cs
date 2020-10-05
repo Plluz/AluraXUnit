629cc51f10c1ff63078b000d9c6f326aac352486
Pedro Lumertz <plluz@outlook.com>
2020-10-05 11:15:09 -0300

    add some maths methods to start testing

--- Maths.cs
+++ Maths.cs
@@ -1,6 +1,32 @@
-﻿namespace AluraXUnit
+﻿using System.Linq;
+
+namespace AluraXUnit
 {
     public class Maths
     {
+        public int Sum(params int[] numbers)
+        {
+            var total = 0;
+
+            foreach (var number in numbers)
+                total += number;
+
+            return total;
+        }
+
+        public int Multiply(params int[] numbers)
+        {
+            var total = 0;
+
+            foreach (var number in numbers)
+                total *= number;
+
+            return total;
+        }
+
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
     }
 }
