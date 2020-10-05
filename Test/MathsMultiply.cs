a95d09aaf1a91a93d41d579fdf16fa6cbca0ff69
Pedro Lumertz <plluz@outlook.com>
2020-10-05 15:47:47 -0300

    splitted tests into 2 files and renamed both test methods

---
+++ Test/MathsMultiply.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace AluraXUnit.Test
+{
+    public class MathsMultiply
+    {
+        readonly Maths _maths;
+
+        public MathsMultiply() => _maths = new Maths();
+
+        [Theory]
+        [InlineData(16, new int[] { 8, 2 })]
+        [InlineData(-25, new int[] { 5, -5 })]
+        [InlineData(120, new int[] { 2, 3, 4, 5 })]
+        public void Should_MultiplyEveryParameter_When_EveryParameterIsValid(int expected, int[] values)
+        {
+            // Act
+            var result = 1;
+            foreach (var value in values)
+                result = _maths.Multiply(result, value);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Test/MathsSum.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace AluraXUnit.Test
+{
+    public class MathsSum
+    {
+        readonly Maths _maths;
+
+        public MathsSum() => _maths = new Maths();
+
+        [Theory]
+        [InlineData(10, new int[] { 5, 5 })]
+        [InlineData(14, new int[] { 20, -6 })]
+        [InlineData(17, new int[] { 10, 2, 5 })]
+        public void Should_SumEveryParameter_When_EveryParameterIsValid(int expected, int[] values)
+        {
+            // Act
+            var result = 0;
+            foreach (var value in values)
+                result = _maths.Sum(result, value);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
